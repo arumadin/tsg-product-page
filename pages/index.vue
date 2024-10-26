67516b9f1df4dd0972550070cee6f3e3860633dd
Arum <arum.adiningtyas@gmail.com>
2024-10-26 16:34:49 +0700

    [Product List] add animation

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
         <Category></Category>
         <div class="container">
             <h3>Our Products</h3>
-            <div class="container product-list">
+            <div class="container product-list" ref="productList">
                 <template v-if="productStore.filteredProduct.length > 0">
                     <Card v-for="(item, index) in productStore.filteredProduct" :key="index" :product="item">
                     </Card>
@@ -20,9 +20,26 @@
 
 <script setup lang="ts">
 import { useProductsStore } from '~/stores/products';
+import gsap from 'gsap';
+
+// get data from Products Store
 const productStore = useProductsStore()
 productStore.getProducts()
 const data = productStore.allProducts
+
+// Animation
+const productList = ref()
+
+onMounted(() => {
+
+    gsap.from(productList.value.children, {
+        delay: 0.5,
+        duration: 1,
+        y: '100',
+        autoAlpha: 0,
+        stagger: 0.25,
+    })
+})
 </script>
 
 <style lang="scss" scoped>

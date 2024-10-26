ae2d069ca04da594e321a511417d12ea88330c38
Arum <arum.adiningtyas@gmail.com>
2024-10-26 13:36:18 +0700

    [Products Store] add filtered products state

--- stores/products.js
+++ stores/products.js
@@ -2,18 +2,30 @@ import { defineStore } from 'pinia'
 
 export const useProductsStore = defineStore('products', {
     state: () => ({
-        products: []
+        products: [],
+        filteredProducts: []
     }),
     getters: {
         allProducts() {
             return this.products
+        },
+        filteredProduct() {
+            return this.filteredProducts
         }
     },
     actions: {
         async getProducts() {
-            const {data, error} = await useFetch('https://my-json-server.typicode.com/arumadin/tsg-product-db/products')
+            const {data, error, status} = await useFetch('https://my-json-server.typicode.com/arumadin/tsg-product-db/products')
             this.products = data.value
         },
+        filter(category) {
+            if (category === 'Show All') {
+                this.filteredProducts = []
+            }
+
+            let data = this.products.filter((p) => p.category === category)
+            this.filteredProducts = data
+        }
     }
 
 })

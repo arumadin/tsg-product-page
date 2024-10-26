2e02a7ec41f57305b909f8da49af93218c0b1da0
Arum <arum.adiningtyas@gmail.com>
2024-10-26 13:38:40 +0700

    [Category] Update category component

--- components/Category/Item.vue
+++ components/Category/Item.vue
@@ -1,18 +1,45 @@
 <template>
-    <div class="category-item">
-        Category
+    <div class="category-item" @click="productStore.filter(category.title)">
+        <p class="category__label">
+            {{ category.title }}
+        </p>
+        <Icon :name="category.icon"></Icon>
     </div>
 </template>
 
 <script setup lang="ts">
-
+import { useProductsStore } from '~/stores/products';
+const props = defineProps(["category"])
+const productStore = useProductsStore()
 </script>
 
 <style lang="scss" scoped>
 .category-item {
     width: 100px;
     height: 100px;
-    border-radius: 20px;
-    background-color: cadetblue;
+    border-radius: 50%;
+    cursor: pointer;
+    position: relative;
+    background-color: $pale-yellow;
+
+    .iconify {
+        font-size: 40px;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+
+    }
+}
+
+.category__label {
+    padding: 5px 15px;
+    position: absolute;
+    width: 100%;
+    bottom: -10px;
+    left: 50%;
+    transform: translate(-50%);
+    text-align: center;
+    margin: 0;
 }
 </style>
--- components/Category/index.vue
+++ components/Category/index.vue
@@ -2,19 +2,53 @@
     <div class="container">
         <h3>Categories</h3>
         <div class="category">
-            <CategoryItem v-for="i in 10"></CategoryItem>
+            <CategoryItem v-for="(category, index) in categories" :key="index" :category="category"></CategoryItem>
         </div>
     </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
+let categories = ref([
+    {
+        title: "Fruit",
+        icon: "emojione:tangerine"
+    },
+    {
+        title: "Baking",
+        icon: "emojione:bread"
+    },
+    {
+        title: "Biscuit",
+        icon: "emojione:cookie"
+    },
+    {
+        title: "Granola",
+        icon: "emojione:honey-pot"
+    },
+    {
+        title: "Show All",
+        icon: "emojione:strawberry"
+    },
+    {
+        title: "Snack",
+        icon: "emojione:popcorn"
+    },
+    {
+        title: "Tea",
+        icon: "emojione:teacup-without-handle"
+    },
+    {
+        title: "Yogurt",
+        icon: "emojione:glass-of-milk"
+    }
+])
 
 </script>
 
 <style lang="scss" scoped>
 .category {
     display: grid;
-    grid-template-columns: repeat(7, 1fr);
+    grid-template-columns: repeat(8, 1fr);
     gap: 20px;
     margin-bottom: 50px;
 }
